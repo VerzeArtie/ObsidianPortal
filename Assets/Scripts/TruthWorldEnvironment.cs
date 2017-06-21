f881581b2b630a6a61783f06d6473e20cc5be83b
verze.artie <verze.artie@gmail.com>
2017-06-21 11:32:35 +0900

    exchange worldenvironment

--- Assets/Scripts/TruthWorldEnvironment.cs
+++ Assets/Scripts/TruthWorldEnvironment.cs
@@ -7,60 +7,65 @@ namespace ObsidianPortal
 {
     public class TruthWorldEnvironment : MonoBehaviour
     {
-        public int TotalPlayed = 0;
-        public int TotalWin = 0;
-        public int TotalLose = 0;
+        public int TotalPlayed { get; set; }
+        public int TotalWin { get; set; }
+        public int TotalLose { get; set; }
 
-        public int MostDamageDone = 0;
-        public int MostHealingDone = 0;
-        public int MostTacticsPoint = 0;
-        public int MostEliminations = 0;
+        public int MostDamageDone { get; set; }
+        public int MostHealingDone { get; set; }
+        public int MostTacticsPoint { get; set; }
+        public int MostEliminations { get; set; }
 
-        public int PlayHuman = 0;
-        public int PlayMech = 0;
-        public int PlayAngel = 0;
-        public int PlayDemon = 0;
-        public int PlayFire = 0;
-        public int PlayIce = 0;
+        public int PlayHuman { get; set; }
+        public int PlayMech { get; set; }
+        public int PlayAngel { get; set; }
+        public int PlayDemon { get; set; }
+        public int PlayFire { get; set; }
+        public int PlayIce { get; set; }
 
-        public int UseDash = 0;
-        public int UseTarget = 0;
-        public int UseEarthBind = 0;
-        public int UseHealingWord = 0;
-        public int UsePowerWord = 0;
+        public int UseDash { get; set; }
+        public int UseTarget { get; set; }
+        public int UseEarthBind { get; set; }
+        public int UseHealingWord { get; set; }
+        public int UsePowerWord { get; set; }
 
-        public int UseNeedSpear = 0;
-        public int UseSilverArrow = 0;
-        public int UseHolyField = 0;
-        public int UseFreshHeal = 0;
-        public int UseProtection = 0;
+        public int UseNeedSpear { get; set; }
+        public int UseSilverArrow { get; set; }
+        public int UseHolyField { get; set; }
+        public int UseFreshHeal { get; set; }
+        public int UseProtection { get; set; }
 
-        public int UseFireBlade = 0;
-        public int UseBlaze = 0;
-        public int UseLavaWall = 0;
-        public int UseHeatBoost = 0;
-        public int UseExplosion = 0;
+        public int UseFireBlade { get; set; }
+        public int UseBlaze { get; set; }
+        public int UseLavaWall { get; set; }
+        public int UseHeatBoost { get; set; }
+        public int UseExplosion { get; set; }
 
-        public bool CompleteStage11 = false;
-        public bool CompleteStage12 = false;
-        public bool CompleteStage13 = false;
-        public bool CompleteStage14 = false;
-        public bool CompleteStage15 = false;
-        public bool CompleteStage21 = false;
-        public bool CompleteStage22 = false;
-        public bool CompleteStage23 = false;
-        public bool CompleteStage24 = false;
-        public bool CompleteStage25 = false;
-        public bool CompleteStage31 = false;
-        public bool CompleteStage32 = false;
-        public bool CompleteStage33 = false;
-        public bool CompleteStage34 = false;
-        public bool CompleteStage35 = false;
-        public bool CompleteStage41 = false;
-        public bool CompleteStage42 = false;
-        public bool CompleteStage43 = false;
-        public bool CompleteStage44 = false;
-        public bool CompleteStage45 = false;
+        public bool CompleteStage11 { get; set; }
+        public bool CompleteStage12 { get; set; }
+        public bool CompleteStage13 { get; set; }
+        public bool CompleteStage14 { get; set; }
+        public bool CompleteStage15 { get; set; }
+        public bool CompleteStage21 { get; set; }
+        public bool CompleteStage22 { get; set; }
+        public bool CompleteStage23 { get; set; }
+        public bool CompleteStage24 { get; set; }
+        public bool CompleteStage25 { get; set; }
+        public bool CompleteStage31 { get; set; }
+        public bool CompleteStage32 { get; set; }
+        public bool CompleteStage33 { get; set; }
+        public bool CompleteStage34 { get; set; }
+        public bool CompleteStage35 { get; set; }
+        public bool CompleteStage41 { get; set; }
+        public bool CompleteStage42 { get; set; }
+        public bool CompleteStage43 { get; set; }
+        public bool CompleteStage44 { get; set; }
+        public bool CompleteStage45 { get; set; }
+        public bool CompleteStage51 { get; set; }
+        public bool CompleteStage52 { get; set; }
+        public bool CompleteStage53 { get; set; }
+        public bool CompleteStage54 { get; set; }
+        public bool CompleteStage55 { get; set; }
 
         // SQLログ採取用アカウント
         public string Account { get; set; }
